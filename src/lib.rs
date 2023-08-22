c59b3f37f3244ab4c93d21d8fb4223c289e196e0
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-08-21 18:15:52 -0700

    add: exit + nums

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 
 #[no_mangle]
-pub extern "C" fn coitusinterruptus(x: *const i8) {
-    let c_str = unsafe { CStr::from_ptr(x) };
-    println!("{}", c_str.to_str().unwrap());
+pub extern "C" fn coitusinterruptus(ptr: *const i8) {
+    println!("{}", unsafe { CStr::from_ptr(ptr).to_str().unwrap() });
+}
+
+#[no_mangle]
+pub extern "C" fn nums(x: f64) -> *const i8 {
+    let c_str = CString::new(x.to_string()).unwrap();
+    let ptr = c_str.as_ptr();
+    ptr
+}
+
+#[no_mangle]
+pub extern "C" fn exit(code: f64) -> ! {
+    // RedditLang uses f64's internally, must convert to i32
+    std::process::exit(code as i32)
 }
